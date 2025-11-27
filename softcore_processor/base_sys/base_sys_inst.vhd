911c69acfc61951ccb514dd8f1bd9d19cba182f9
AGILAN V S <63512427+Lan15@users.noreply.github.com>
2025-11-27 11:55:46 +0100

    Initial update
    Nios II Hardware design component placement done in the platform designer.

---
+++ softcore_processor/base_sys/base_sys_inst.vhd
@@ -0,0 +1,13 @@
+	component base_sys is
+		port (
+			clk_clk       : in std_logic := 'X'; -- clk
+			reset_reset_n : in std_logic := 'X'  -- reset_n
+		);
+	end component base_sys;
+
+	u0 : component base_sys
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
+			reset_reset_n => CONNECTED_TO_reset_reset_n  -- reset.reset_n
+		);
+
